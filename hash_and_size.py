1fd74faa4df8d7185d77d11a8169d4d43f04391b
wave-rider <wave-rider@users.noreply.github.com>
2023-12-13 23:28:17 +1300

    Added some error handling
    and a continue option (not finished yet)

--- hash_and_size.py
+++ hash_and_size.py
@@ -3,6 +3,8 @@ import hashlib
 import csv
 import unicodedata
 import sys
+import time
+# F:\ D:\Drive_F.csv
 
 def hash_file(file_path):
     sha256_hash = hashlib.sha256()
@@ -16,7 +18,14 @@ def normalize_file_name(file_name):
     # Normalize to NFC (composed) form
     return unicodedata.normalize('NFC', file_name)
 def process_directory(directory_path, output_file):
-    with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
+    existing_data = set()
+    #start_time = time.time()
+    if os.path.isfile(output_file):
+        with open(output_file, 'r', encoding='UTF-8') as f1:
+            for line in f1:
+                name = line.strip().split('|')[0]
+                existing_data.add(name)
+    with open(output_file, 'a', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter='|')
         #csv_writer.writerow(['Full File Path', 'File Hash', 'File Size'])
 
@@ -25,10 +34,18 @@ def process_directory(directory_path, output_file):
                 file_path = os.path.join(foldername, filename)
                 #this is needed for MacOS
                 #file_path = normalize_file_name(the_name)
-                file_size = os.path.getsize(file_path)
-                file_hash = hash_file(file_path)
-                print(file_path, file_hash, file_size)
-                csv_writer.writerow([file_path, file_hash, file_size])
+                if (file_path not in existing_data) and os.path.exists(file_path):
+
+                    file_size = os.path.getsize(file_path)
+                    file_hash = ""
+                    try:
+                        file_hash = hash_file(file_path)
+                        print(file_path, file_hash, file_size)
+                    except Exception as e:
+                        file_path="Error_path"
+                        file_hash = f"Error occured {e}"
+
+                    csv_writer.writerow([file_path, file_hash, file_size])
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -36,4 +53,5 @@ if __name__ == "__main__":
         sys.exit(1)
     directory_path = sys.argv[1]
     output_file = sys.argv[2]
+
     process_directory(directory_path, output_file)
